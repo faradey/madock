33ea8ceee87b82cab1daad3bf2b4e5a27567638b
Faradey <rvansp@gmail.com>
2021-09-16 23:34:29 +0300

    add paths package

--- main.go
+++ main.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
+	"github.com/faradey/madock/src/paths"
 )
 
 func main() {
-	fmt.Println(getDir())
-}
-
-func getDir() string {
-	var dirAbsPath string
-	ex, err := os.Executable()
-	if err == nil {
-		dirAbsPath = filepath.Dir(ex)
-		return dirAbsPath
-	}
-
-	exReal, err := filepath.EvalSymlinks(ex)
-	if err != nil {
-		panic(err)
-	}
-	dirAbsPath = filepath.Dir(exReal)
-	return dirAbsPath
+	fmt.Println(paths.GetExecDirPath())
+	fmt.Println(paths.GetRunDirName())
 }
