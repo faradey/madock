7a064ddccd96f3560e33d33747e2ee355468cfbd
Faradey <rvansp@gmail.com>
2021-09-23 23:44:23 +0300

    change .gitignore

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"github.com/faradey/madock/src/configs"
 	"github.com/faradey/madock/src/paths"
+	"os"
 )
 
 func main() {
-	fmt.Println(paths.GetExecDirName())
-	fmt.Println(paths.GetExecDirPath())
-	fmt.Println(paths.GetRunDirPath())
-	fmt.Println(paths.GetRunDirName())
-	fmt.Println(configs.GetProjectsConfig(paths.GetExecDirPath() + "/projects"))
+	if len(os.Args) > 0 {
+		command := os.Args[1]
+		fmt.Println(command)
+		fmt.Println(paths.GetExecDirName())
+		fmt.Println(paths.GetExecDirPath())
+		fmt.Println(paths.GetRunDirPath())
+		fmt.Println(paths.GetRunDirName())
+		fmt.Println(configs.GetProjectsConfig(paths.GetExecDirPath() + "/projects"))
+	}
 }
