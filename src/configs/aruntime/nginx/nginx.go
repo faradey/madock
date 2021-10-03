efc6d46d99b09d3ff13757a881962768b3bb1cd5
Faradey <rvansp@gmail.com>
2021-10-03 21:08:03 +0300

    add db config to aruntime

--- src/configs/aruntime/nginx/nginx.go
+++ src/configs/aruntime/nginx/nginx.go
@@ -1,6 +1,7 @@
 package nginx
 
 import (
+	"github.com/faradey/madock/src/configs"
 	"github.com/faradey/madock/src/paths"
 	"io/ioutil"
 	"log"
@@ -11,12 +12,43 @@ import (
 
 func MakeConf() {
 	paths.MakeDirsByPath(paths.GetExecDirPath() + "/projects/" + paths.GetRunDirName() + "/docker/nginx")
+	setPorts()
 	makeProxy()
 	makeDockerfile()
 	makeDockerCompose()
 }
 
+func setPorts() {
+	projects := paths.GetDirs(paths.GetExecDirPath() + "/aruntime/projects")
+	portsFile := paths.GetExecDirPath() + "/aruntime/ports.conf"
+	portsConfig := make(map[string]string)
+	if _, err := os.Stat(portsFile); os.IsNotExist(err) {
+		lines := ""
+		for port, line := range projects {
+			lines += line + "=" + strconv.Itoa(17000+port) + "\n"
+		}
+		err = ioutil.WriteFile(portsFile, []byte(lines), 0755)
+	}
+
+	portsConfig = configs.ParseFile(portsFile)
+
+	if _, ok := portsConfig[paths.GetRunDirName()]; !ok {
+		f, err := os.OpenFile(portsFile,
+			os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+		maxPort := getMaxPort(portsConfig)
+		if _, err := f.WriteString(paths.GetRunDirName() + "=" + strconv.Itoa(maxPort+1) + "\n"); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
 func makeProxy() {
+	portsFile := paths.GetExecDirPath() + "/aruntime/ports.conf"
+	portsConfig := configs.ParseFile(portsFile)
 	/* Create nginx default configuration for Magento2 */
 	nginxDefFile := paths.GetExecDirPath() + "/docker/nginx/conf/default-proxy.conf"
 	allFileData := ""
@@ -26,9 +58,9 @@ func makeProxy() {
 	}
 	str := string(b)
 	projectsNames := paths.GetDirs(paths.GetExecDirPath() + "/aruntime/projects")
-	for index, name := range projectsNames {
+	for _, name := range projectsNames {
 		if _, err := os.Stat(paths.GetExecDirPath() + "/aruntime/projects/" + name + "/stopped"); os.IsNotExist(err) {
-			strReplaced := strings.Replace(str, "{{{NGINX_PORT}}}", strconv.Itoa(index+17000), -1)
+			strReplaced := strings.Replace(str, "{{{NGINX_PORT}}}", portsConfig[name], -1)
 			strReplaced = strings.Replace(strReplaced, "{{{HOST_NAMES}}}", "loc."+name+".com", -1)
 			allFileData += strReplaced
 		}
@@ -73,4 +105,21 @@ func makeDockerCompose() {
 		log.Fatalf("Unable to write file: %v", err)
 	}
 	/* END Create nginx Dockerfile configuration */
+}
+
+func getMaxPort(conf map[string]string) int {
+	max := 0
+	portInt := 0
+	var err error
+	for _, port := range conf {
+		portInt, err = strconv.Atoi(port)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if max < portInt {
+			max = portInt
+		}
+	}
+
+	return max
 }
--- src/configs/aruntime/project/project.go
+++ src/configs/aruntime/project/project.go
@@ -24,9 +24,11 @@ func MakeConf(projectName string) {
 		log.Fatal(err)
 	}
 
+	makeDockerCompose(projectName)
 	makeNginxDockerfile(projectName)
 	makeNginxConf(projectName)
 	makePhpDockerfile(projectName)
+	makeDBDockerfile(projectName)
 }
 
 func makeNginxDockerfile(projectName string) {
@@ -95,6 +97,78 @@ func makePhpDockerfile(projectName string) {
 	paths.MakeDirsByPath(paths.GetExecDirPath() + "/aruntime/projects/" + projectName + "/ctx")
 	nginxFile := paths.GetExecDirPath() + "/aruntime/projects/" + projectName + "/ctx/php.Dockerfile"
 	err = ioutil.WriteFile(nginxFile, []byte(str), 0755)
+	if err != nil {
+		log.Fatalf("Unable to write file: %v", err)
+	}
+}
+
+func makeDockerCompose(projectName string) {
+	dockerDefFile := paths.GetExecDirPath() + "/docker/docker-compose.yml"
+	if _, err := os.Stat(dockerDefFile); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	b, err := os.ReadFile(dockerDefFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(b)
+	portsConfig := configs.ParseFile(paths.GetExecDirPath() + "/aruntime/ports.conf")
+	str = strings.Replace(str, "{{{NGINX_PORT}}}", portsConfig[projectName], -1)
+
+	paths.MakeDirsByPath(paths.GetExecDirPath() + "/aruntime/projects/" + projectName)
+	nginxFile := paths.GetExecDirPath() + "/aruntime/projects/" + projectName + "/docker-compose.yml"
+	err = ioutil.WriteFile(nginxFile, []byte(str), 0755)
+	if err != nil {
+		log.Fatalf("Unable to write file: %v", err)
+	}
+}
+
+func makeDBDockerfile(projectName string) {
+	dockerDefFile := paths.GetExecDirPath() + "/docker/db/Dockerfile"
+	if _, err := os.Stat(dockerDefFile); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	b, err := os.ReadFile(dockerDefFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	projectConf := configs.GetProjectConfig()
+	str := string(b)
+	str = strings.Replace(str, "{{{DB_VERSION}}}", projectConf["DB_VERSION"], -1)
+	str = strings.Replace(str, "{{{DB_ROOT_PASSWORD}}}", projectConf["DB_ROOT_PASSWORD"], -1)
+	str = strings.Replace(str, "{{{DB_DATABASE}}}", projectConf["DB_DATABASE"], -1)
+	str = strings.Replace(str, "{{{DB_USER}}}", projectConf["DB_USER"], -1)
+	str = strings.Replace(str, "{{{DB_PASSWORD}}}", projectConf["DB_PASSWORD"], -1)
+
+	usr, err := user.Current()
+	if err == nil {
+		str = strings.Replace(str, "{{{UID}}}", usr.Uid, -1)
+		str = strings.Replace(str, "{{{GUID}}}", usr.Gid, -1)
+	} else {
+		log.Fatal(err)
+	}
+	paths.MakeDirsByPath(paths.GetExecDirPath() + "/aruntime/projects/" + projectName + "/ctx")
+	nginxFile := paths.GetExecDirPath() + "/aruntime/projects/" + projectName + "/ctx/db.Dockerfile"
+	err = ioutil.WriteFile(nginxFile, []byte(str), 0755)
+	if err != nil {
+		log.Fatalf("Unable to write file: %v", err)
+	}
+
+	myCnfFile := paths.GetExecDirPath() + "/docker/db/my.cnf"
+	if _, err := os.Stat(myCnfFile); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	b, err = os.ReadFile(myCnfFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(paths.GetExecDirPath()+"/aruntime/projects/"+projectName+"/ctx/my.cnf", b, 0755)
 	if err != nil {
 		log.Fatalf("Unable to write file: %v", err)
 	}
