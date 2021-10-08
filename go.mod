e794a714a362487958f636daf7c06c2d347ebdc2
faradey <rvansp@gmail.com>
2021-10-09 01:04:36 +0300

    import export database completed

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/faradey/madock
 
 go 1.16
+
+require (
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/pipe.v2 v2.0.0-20140414041502-3c2ca4d52544
+)
--- src/docker/builder/build.go
+++ src/docker/builder/build.go
@@ -2,7 +2,6 @@ package builder
 
 import (
 	"bufio"
-	"bytes"
 	"compress/gzip"
 	"fmt"
 	"github.com/faradey/madock/src/configs"
@@ -14,6 +13,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func Up() {
@@ -116,41 +116,55 @@ func DbImport(option string) {
 
 	ext := dbNames[selectedInt-1][len(dbNames[selectedInt-1])-2:]
 	out := &gzip.Reader{}
+
+	selectedFile, err := os.Open(dbsPath + "/" + dbNames[selectedInt-1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer selectedFile.Close()
+
+	var cmd *exec.Cmd
+	if option != "" {
+		cmd = exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", option, "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", projectConfig["DB_DATABASE"])
+	} else {
+		cmd = exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", projectConfig["DB_DATABASE"])
+	}
+
 	if ext == "gz" {
-		selectedFile, err := os.Open(dbsPath + "/" + dbNames[selectedInt-1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer selectedFile.Close()
 		out, err = gzip.NewReader(selectedFile)
 		if err != nil {
 			log.Fatal(err)
 		}
-		cmd := exec.Command("docker", "exec", "-u", "mysql", projectName+"-db-1", "mysql", option, "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", projectConfig["DB_DATABASE"])
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		/*stdin, err := cmd.StdinPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stdin.Close()
-		io.Copy(stdin, out)*/
-		buffer := bytes.Buffer{}
-		buffer.ReadFrom(out)
-		cmd.Stdin = &buffer
-		cmd.Run()
+		cmd.Stdin = out
 	} else {
-		cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", "-t", option, "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", projectConfig["DB_DATABASE"], "<", dbsPath+"/"+dbNames[selectedInt-1])
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
+		cmd.Stdin = selectedFile
 	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Database import completed successfully")
 }
 
 func DbExport() {
 	projectName := paths.GetRunDirName()
-	cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"_php_1", "mysqldump", "-t", "cd /var/www/html && composer ")
+	projectConfig := configs.GetProjectConfig()
+	dbsPath := paths.GetExecDirPath() + "/projects/" + projectName + "/backup/db/"
+	selectedFile, err := os.Create(dbsPath + time.Now().Format("2006-01-02_15-04-05") + ".sql.gz")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer selectedFile.Close()
+	writer := gzip.NewWriter(selectedFile)
+	cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysqldump", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", projectConfig["DB_DATABASE"])
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
+	cmd.Stdout = writer
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Database export completed successfully")
 }
