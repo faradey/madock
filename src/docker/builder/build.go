2c261228c357c3d57174baebdbbbbd21befc21c2
faradey <rvansp@gmail.com>
2021-10-24 00:35:12 +0300

    db soft-clean

--- src/docker/builder/build.go
+++ src/docker/builder/build.go
@@ -333,7 +333,7 @@ func DbSoftClean() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tbNames := strings.Split(fmt.Sprintf("%s", b), "\n")
+	tbNames := strings.Split(fmt.Sprint(b), "\n")
 	fmt.Println(tbNames)
 	cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", "--execute", tablesList, "-f", projectConfig["DB_DATABASE"])
 	err = cmd.Run()
