d7689db6e37da88c870dcf5a72b16e2766db8bb3
faradey <rvansp@gmail.com>
2021-10-24 00:36:04 +0300

    db soft-clean

--- src/docker/builder/build.go
+++ src/docker/builder/build.go
@@ -326,14 +326,15 @@ func DbSoftClean() {
 	tablesList += "TRUNCATE TABLE mailchimp_errors;"
 
 	var b io.Writer
+	var e io.Writer
 	cmdTemp := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", "-e", "SHOW TABLES LIKE \"catalogrule_product%__temp%\"", projectConfig["DB_DATABASE"])
 	cmdTemp.Stdout = b
-	cmdTemp.Stderr = os.Stderr
+	cmdTemp.Stderr = e
 	err := cmdTemp.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-	tbNames := strings.Split(fmt.Sprint(b), "\n")
+	tbNames := strings.Split(fmt.Sprint(b)+fmt.Sprint(e), "\n")
 	fmt.Println(tbNames)
 	cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"-db-1", "mysql", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-h", "db", "--execute", tablesList, "-f", projectConfig["DB_DATABASE"])
 	err = cmd.Run()
