aa4fc39c3902184a4b15f6da69f29ce8f04dafb8
faradey <rvansp@gmail.com>
2021-11-09 23:48:13 +0200

    add verbos for mysqldump

--- src/docker/builder/build.go
+++ src/docker/builder/build.go
@@ -346,6 +346,7 @@ func DbExport() {
 	}
 	defer selectedFile.Close()
 	writer := gzip.NewWriter(selectedFile)
+	defer writer.Close()
 	cmd := exec.Command("docker", "exec", "-i", "-u", "mysql", projectName+"_db_1", "mysqldump", "-u", "root", "-p"+projectConfig["DB_ROOT_PASSWORD"], "-v", "-h", "db", projectConfig["DB_DATABASE"])
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
