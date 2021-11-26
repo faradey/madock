48a941a83118045cc7d944d885f1968ec31de68f
faradey <rvansp@gmail.com>
2021-11-27 00:18:51 +0200

    improve ssh for auth by password

--- src/ssh/ssher.go
+++ src/ssh/ssher.go
@@ -54,26 +54,22 @@ func DbDump(conn *ssh.Client, remoteDir string) {
 }
 
 func Connect(authType, keyPath, passwd, host, port, username string) *ssh.Client {
-	config := &ssh.ClientConfig{}
 	var sshAuth []ssh.AuthMethod
 
 	if authType == "password" {
 		sshAuth = []ssh.AuthMethod{
 			ssh.Password(passwd),
 		}
-		config = &ssh.ClientConfig{
-			User: username,
-			Auth: sshAuth,
-		}
 	} else {
 		sshAuth = []ssh.AuthMethod{
 			publicKey(keyPath),
 		}
-		config = &ssh.ClientConfig{
-			User:            username,
-			Auth:            sshAuth,
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		}
+	}
+
+	config := &ssh.ClientConfig{
+		User:            username,
+		Auth:            sshAuth,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
 	conn, err := ssh.Dial("tcp", host+":"+port, config)
