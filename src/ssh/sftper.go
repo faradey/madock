39e1abd4e3a48b533d7e1ccb0de501cd3d94abbb
faradey <rvansp@gmail.com>
2021-11-30 23:26:33 +0200

    downloading media files by several streams

--- src/ssh/sftper.go
+++ src/ssh/sftper.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"fmt"
+	"github.com/faradey/madock/src/configs"
 	"github.com/faradey/madock/src/paths"
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
@@ -15,24 +16,41 @@ import (
 )
 
 var countGoroutine int
+var sc *sftp.Client
+var sc2 *sftp.Client
 
 func Sync(conn *ssh.Client, remoteDir string) {
-	sc, err := sftp.NewClient(conn)
+	var err error
+	sc, err = sftp.NewClient(conn)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	projectConfig := configs.GetCurrentProjectConfig()
+	conn2 := Connect(projectConfig["SSH_AUTH_TYPE"], projectConfig["SSH_KEY_PATH"], projectConfig["SSH_PASSWORD"], projectConfig["SSH_HOST"], projectConfig["SSH_PORT"], projectConfig["SSH_USERNAME"])
+	sc2, err = sftp.NewClient(conn2)
+	if err != nil {
+		fmt.Println(err)
 	}
 	countGoroutine = 0
 	ch := make(chan bool, 50)
 	fmt.Println("Synchronization is started")
-	listFiles(sc, ch, remoteDir+"/pub/media/", "", 0)
+	listFiles(ch, remoteDir+"/pub/media/", "", 0)
 
 	defer sc.Close()
+	defer sc2.Close()
 	defer Disconnect(conn)
+	defer Disconnect(conn2)
 }
 
-func listFiles(sc *sftp.Client, ch chan bool, remoteDir, subdir string, isFirst int) (err error) {
+func listFiles(ch chan bool, remoteDir, subdir string, isFirst int) (err error) {
+	scp := sc
+	remainder := countGoroutine % 2
+	if remainder == 1 {
+		scp = sc2
+	}
 	projectPath := paths.GetRunDirPath()
-	files, err := sc.ReadDir(remoteDir + subdir)
+	files, err := scp.ReadDir(remoteDir + subdir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -53,24 +71,18 @@ func listFiles(sc *sftp.Client, ch chan bool, remoteDir, subdir string, isFirst 
 				}
 
 				if countGoroutine <= 5 || isFirst == 0 {
-					//projectConfig := configs.GetCurrentProjectConfig()
-					//conn := Connect(projectConfig["SSH_AUTH_TYPE"], projectConfig["SSH_KEY_PATH"], projectConfig["SSH_PASSWORD"], projectConfig["SSH_HOST"], projectConfig["SSH_PORT"], projectConfig["SSH_USERNAME"])
-					/*sc2, err := sftp.NewClient(conn)
-					if err != nil {
-						fmt.Println(err)
-					}*/
 					countGoroutine++
-					go listFiles(sc, ch, remoteDir, subdir+name+"/", isFirst+1)
+					go listFiles(ch, remoteDir, subdir+name+"/", isFirst+1)
 				} else {
 					countGoroutine++
-					listFiles(sc, ch, remoteDir, subdir+name+"/", isFirst+1)
+					listFiles(ch, remoteDir, subdir+name+"/", isFirst+1)
 				}
 			}
 		} else if _, err := os.Stat(projectPath + "/pub/media/" + subdir + name); os.IsNotExist(err) {
 			ext := strings.ToLower(filepath.Ext(name))
 			if ext == ".jpeg" || ext == ".jpg" || ext == ".png" || ext == ".webp" {
 				fmt.Printf("%s\n", projectPath+"/pub/media/"+subdir+name)
-				downloadFile(sc, remoteDir+"/"+subdir+name, projectPath+"/pub/media/"+subdir+name)
+				downloadFile(scp, remoteDir+"/"+subdir+name, projectPath+"/pub/media/"+subdir+name)
 			}
 		}
 	}
@@ -94,10 +106,10 @@ func listFiles(sc *sftp.Client, ch chan bool, remoteDir, subdir string, isFirst 
 	return
 }
 
-func downloadFile(sc *sftp.Client, remoteFile, localFile string) (err error) {
+func downloadFile(scp *sftp.Client, remoteFile, localFile string) (err error) {
 	ext := strings.ToLower(filepath.Ext(remoteFile))
 	// Note: SFTP To Go doesn't support O_RDWR mode
-	srcFile, err := sc.OpenFile(remoteFile, (os.O_RDONLY))
+	srcFile, err := scp.OpenFile(remoteFile, (os.O_RDONLY))
 	if err != nil {
 		fmt.Println("Unable to open remote file: " + err.Error() + "\n")
 		return
