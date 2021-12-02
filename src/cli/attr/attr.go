27c8b7906c4b486df014652a65d2f4b3b9442cd2
faradey <rvansp@gmail.com>
2021-12-03 00:05:07 +0200

    parse cli attributes

--- src/cli/attr/attr.go
+++ src/cli/attr/attr.go
@@ -5,6 +5,7 @@ import "strconv"
 var Attributes map[string]string
 
 func ParseAttributes(args []string) {
+	Attributes = make(map[string]string)
 	if len(args) > 2 {
 		lastAttribute := ""
 		for i, val := range args[2:] {
