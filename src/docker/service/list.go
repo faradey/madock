645debdca304d83ae3e6eb981b3732a68e832e20
faradey <rvansp@gmail.com>
2022-07-14 22:55:00 +0300

    services list completed

--- src/docker/service/list.go
+++ src/docker/service/list.go
@@ -1,5 +1,30 @@
 package service
 
-func ServiceList() {
+import (
+	"sort"
+	"strings"
 
+	"github.com/faradey/madock/src/cli/fmtc"
+	"github.com/faradey/madock/src/configs"
+)
+
+func ServiceList() {
+	configData := configs.GetCurrentProjectConfig()
+	keys := make([]string, 0, len(configData))
+	for k := range configData {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		serviceName := strings.SplitN(key, "_ENABLED", 2)
+		if serviceName[0] != key {
+			fmtc.Title(strings.ToLower(serviceName[0]))
+			if configData[key] == "true" {
+				fmtc.SuccessLn(" enabled")
+			} else {
+				fmtc.WarningLn(" disabled")
+			}
+		}
+	}
 }
