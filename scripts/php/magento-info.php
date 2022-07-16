ecd971e12dd4843b8ca754815cd48a7b2b28f6e9
faradey <rvansp@gmail.com>
2022-07-16 11:01:55 +0300

    added scripts to repo

---
+++ scripts/php/magento-info.php
@@ -0,0 +1,71 @@
+<?php
+$siteRootPath = "/var/www/html";
+$configPath = $siteRootPath."/app/etc/config.php";
+$composerJson = $siteRootPath."/composer.json";
+$composerLock = $siteRootPath."/composer.lock";
+
+$appCodePath = $siteRootPath."/app/code";
+$vendorPath = $siteRootPath."/vendor";
+
+if(file_exists($configPath)){
+    $configData = include_once($configPath);
+    $modules = $configData['modules'];
+    $psr4 = include_once($vendorPath."/composer/autoload_psr4.php");
+    $composer = file_get_contents($composerJson);
+    $composer = json_decode($composer, true);
+    print("Magento Version\n");
+    if(!empty($composer['require']['magento/product-enterprise-edition'])){
+        $magentoVersion = "Enterprise edition ".$composer['require']['magento/product-enterprise-edition'];
+    } else {
+        $magentoVersion = "Community edition ".$composer['require']['magento/product-community-edition'];
+    }
+    
+    print($magentoVersion."\n\n");
+    print("Third-parties modules\n");
+    print("Name, Current version,  Latest version, Status\n");
+    if(exec("composer show -f json", $latests) !== false){
+        if(!empty($latests)){
+            $latests = json_decode(implode("", $latests), true);
+            $latests = prepareLatest($latests['installed']);
+        }
+    }
+    
+    foreach($modules as $moduleName => $isActive) {
+        if(strpos($moduleName, "Magento_", 0) !== 0){
+            $version = "\"no version\"";
+            $latestVersion = null;
+            $modulePath = $psr4[str_replace("_", "\\", $moduleName)."\\"][0]??false;
+            if($modulePath && file_exists($modulePath."/composer.json")) {
+                $composerData = file_get_contents($modulePath."/composer.json");
+                $composerData = json_decode($composerData, true);
+                if(empty($composer['require'][$composerData['name']])){
+                    continue;
+                }
+                $version = $composerData['version']??$version;
+                $latestVersion = $latests[$composerData['name']]['version']??null;
+            } else {
+                $modVN = explode("_", $moduleName);
+                if(file_exists($appCodePath."/".$modVN[0]."/".$modVN[1]."/composer.json")) {
+                    $composerData = file_get_contents($appCodePath."/".$modVN[0]."/".$modVN[1]."/composer.json");
+                    $composerData = json_decode($composerData, true);
+                    $version = $composerData['version']??$version;
+                }
+            }
+
+            if(!$latestVersion) {
+                $latestVersion = $version;
+            }
+
+            print($moduleName.", ".$version.", ".$latestVersion.", ".($isActive==1?"enabled":"disabled")."\n");
+        }
+    }
+}
+
+function prepareLatest($latests) {
+    $arr = [];
+    foreach($latests as $latest) {
+        $arr[$latest['name']] = $latest;
+    }
+
+    return $arr;
+}
