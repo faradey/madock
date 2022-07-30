3f408ffd1e0e338407c97ebb61c8fe41a7f76b37
faradey <rvansp@gmail.com>
2022-07-30 16:01:28 +0300

    fixed folder projects with install project

--- src/migration/migration.go
+++ src/migration/migration.go
@@ -11,7 +11,7 @@ import (
 var versionOptionName string = "MADOCK_VERSION"
 
 func Apply(newAppVersion string) {
-	configPath := paths.GetExecDirPath() + "/projects/config.txt"
+	configPath := paths.MakeDirsByPath(paths.GetExecDirPath()+"/projects") + "/config.txt"
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		ioutil.WriteFile(configPath, []byte(""), 0755)
 	}
