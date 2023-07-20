6c48690f1f99c85dc8ff8b399bf280bed73ee0d5
faradey <rvansp@gmail.com>
2023-07-20 23:42:12 +0300

    fixed discovering active projects

--- src/paths/paths.go
+++ src/paths/paths.go
@@ -141,14 +141,14 @@ func MakeDirsByPath(val string) string {
 func GetActiveProjects() []string {
 	var activeProjects []string
 	projects := GetDirs(GetExecDirPath() + "/aruntime/projects")
-	for _, projectName := range projects {
-		if _, err := os.Stat(GetExecDirPath() + "/aruntime/projects/" + projectName + "/docker-compose.yml"); !os.IsNotExist(err) {
-			cmd := exec.Command("docker", "compose", "-f", GetExecDirPath()+"/aruntime/projects/"+projectName+"/docker-compose.yml", "ps", "--format", "json")
-			result, err := cmd.CombinedOutput()
-			if err != nil {
-				log.Fatal(err)
-			}
-			if len(result) > 100 {
+	cmd := exec.Command("docker", "ps", "--format", "json")
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		resultString := string(result)
+		for _, projectName := range projects {
+			if strings.Contains(resultString, projectName+"-") {
 				activeProjects = append(activeProjects, projectName)
 			}
 		}
