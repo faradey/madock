0ba2b2ae0984b8b456088bfec09f67bd79e97461
faradey <rvansp@gmail.com>
2023-11-18 11:59:53 +0200

    refactoring code for main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/faradey/madock/src/migration"
 	"log"
 	"os"
 	"strings"
@@ -9,129 +10,129 @@ import (
 	"github.com/faradey/madock/src/cli/commands"
 	"github.com/faradey/madock/src/cli/helper"
 	cliHelper "github.com/faradey/madock/src/helper"
-	"github.com/faradey/madock/src/migration"
 )
 
 var appVersion string = "2.2.0"
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	if len(os.Args) > 1 {
-		migration.Apply(appVersion)
-		command := strings.ToLower(os.Args[1])
-		attr.ParseAttributes()
-
-		switch command {
-		case "bash":
-			commands.Bash()
-		case "c:f":
-			commands.CleanCache()
-		case "magento-cloud", "cloud":
-			commands.Cloud(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "cli":
-			commands.Cli(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "composer":
-			commands.Composer(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "compress":
-			commands.Compress()
-		case "config:list":
-			commands.ShowEnv()
-		case "config:set":
-			commands.SetEnvOption()
-		case "cron:enable":
-			commands.CronEnable()
-		case "cron:disable":
-			commands.CronDisable()
-		case "db:import":
-			commands.DBImport()
-		case "db:export":
-			commands.DBExport()
-		case "db:info":
-			commands.DBInfo()
-		case "debug:enable":
-			commands.DebugEnable()
-		case "debug:disable":
-			commands.DebugDisable()
-		case "debug:profile:enable":
-			commands.DebugProfileEnable()
-		case "debug:profile:disable":
-			commands.DebugProfileDisable()
-		case "info":
-			commands.Info()
-		case "install":
-			commands.InstallMagento()
-		case "help":
-			helper.Help()
-		case "logs":
-			commands.Logs()
-		case "magento", "m":
-			commands.Magento(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "mftf":
-			commands.Mftf(strings.Join(os.Args[2:], " "))
-		case "mftf:init":
-			commands.MftfInit()
-		case "n98":
-			commands.N98(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "node":
-			commands.Node(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "patch:create":
-			commands.PatchCreate()
-		case "project:remove":
-			commands.ProjectRemove()
-		case "proxy:start":
-			commands.Proxy("start")
-		case "proxy:stop":
-			commands.Proxy("stop")
-		case "proxy:restart":
-			commands.Proxy("restart")
-		case "proxy:rebuild":
-			commands.Proxy("rebuild")
-		case "proxy:prune":
-			commands.Proxy("prune")
-		case "prune":
-			commands.Prune()
-		case "rebuild":
-			commands.Rebuild()
-		case "remote:sync:db":
-			commands.RemoteSyncDb()
-		case "remote:sync:media":
-			commands.RemoteSyncMedia()
-		case "remote:sync:file":
-			commands.RemoteSyncFile()
-		case "restart":
-			commands.Restart()
-		case "pwa":
-			commands.PWA(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
-		case "service:list":
-			commands.ServiceList()
-		case "service:enable":
-			commands.ServiceEnable()
-		case "service:disable":
-			commands.ServiceDisable()
-		case "setup":
-			commands.Setup()
-		case "setup:env":
-			commands.SetupEnv()
-		case "shopify", "sy":
-			commands.Shopify(strings.Join(os.Args[2:], " "))
-		case "shopify:web", "syw":
-			commands.ShopifyWeb(strings.Join(os.Args[2:], " "))
-		case "shopify:web:frontend", "sywf":
-			commands.ShopifyWebFrontend(strings.Join(os.Args[2:], " "))
-		case "ssl:rebuild":
-			commands.Ssl()
-		case "start":
-			commands.Start()
-		case "status":
-			commands.Status()
-		case "stop":
-			commands.Stop()
-		case "uncompress":
-			commands.Uncompress()
-		default:
-			commands.IsNotDefine()
-		}
-	} else {
+	migration.Apply(appVersion)
+	if len(os.Args) <= 1 {
 		helper.Help()
+		return
+	}
+
+	command := strings.ToLower(os.Args[1])
+	attr.ParseAttributes()
+
+	switch command {
+	case "bash":
+		commands.Bash()
+	case "c:f":
+		commands.CleanCache()
+	case "magento-cloud", "cloud":
+		commands.Cloud(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "cli":
+		commands.Cli(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "composer":
+		commands.Composer(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "compress":
+		commands.Compress()
+	case "config:list":
+		commands.ShowEnv()
+	case "config:set":
+		commands.SetEnvOption()
+	case "cron:enable":
+		commands.CronEnable()
+	case "cron:disable":
+		commands.CronDisable()
+	case "db:import":
+		commands.DBImport()
+	case "db:export":
+		commands.DBExport()
+	case "db:info":
+		commands.DBInfo()
+	case "debug:enable":
+		commands.DebugEnable()
+	case "debug:disable":
+		commands.DebugDisable()
+	case "debug:profile:enable":
+		commands.DebugProfileEnable()
+	case "debug:profile:disable":
+		commands.DebugProfileDisable()
+	case "info":
+		commands.Info()
+	case "install":
+		commands.InstallMagento()
+	case "help":
+		helper.Help()
+	case "logs":
+		commands.Logs()
+	case "magento", "m":
+		commands.Magento(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "mftf":
+		commands.Mftf(strings.Join(os.Args[2:], " "))
+	case "mftf:init":
+		commands.MftfInit()
+	case "n98":
+		commands.N98(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "node":
+		commands.Node(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "patch:create":
+		commands.PatchCreate()
+	case "project:remove":
+		commands.ProjectRemove()
+	case "proxy:start":
+		commands.Proxy("start")
+	case "proxy:stop":
+		commands.Proxy("stop")
+	case "proxy:restart":
+		commands.Proxy("restart")
+	case "proxy:rebuild":
+		commands.Proxy("rebuild")
+	case "proxy:prune":
+		commands.Proxy("prune")
+	case "prune":
+		commands.Prune()
+	case "rebuild":
+		commands.Rebuild()
+	case "remote:sync:db":
+		commands.RemoteSyncDb()
+	case "remote:sync:media":
+		commands.RemoteSyncMedia()
+	case "remote:sync:file":
+		commands.RemoteSyncFile()
+	case "restart":
+		commands.Restart()
+	case "pwa":
+		commands.PWA(cliHelper.NormalizeCliCommandWithJoin(os.Args[2:]))
+	case "service:list":
+		commands.ServiceList()
+	case "service:enable":
+		commands.ServiceEnable()
+	case "service:disable":
+		commands.ServiceDisable()
+	case "setup":
+		commands.Setup()
+	case "setup:env":
+		commands.SetupEnv()
+	case "shopify", "sy":
+		commands.Shopify(strings.Join(os.Args[2:], " "))
+	case "shopify:web", "syw":
+		commands.ShopifyWeb(strings.Join(os.Args[2:], " "))
+	case "shopify:web:frontend", "sywf":
+		commands.ShopifyWebFrontend(strings.Join(os.Args[2:], " "))
+	case "ssl:rebuild":
+		commands.Ssl()
+	case "start":
+		commands.Start()
+	case "status":
+		commands.Status()
+	case "stop":
+		commands.Stop()
+	case "uncompress":
+		commands.Uncompress()
+	default:
+		commands.IsNotDefine()
 	}
 }
