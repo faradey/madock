500233f07d5aff17501ffd157857c811e8628b9c
faradey <rvansp@gmail.com>
2024-05-17 17:31:14 +0300

    Fixed db:info

--- src/controller/general/db/info/info.go
+++ src/controller/general/db/info/info.go
@@ -1,6 +1,7 @@
 package info
 
 import (
+	"fmt"
 	"github.com/faradey/madock/src/helper/cli/attr"
 	"github.com/faradey/madock/src/helper/cli/fmtc"
 	configs2 "github.com/faradey/madock/src/helper/configs"
@@ -24,12 +25,22 @@ func Info() {
 		if err != nil {
 			logger.Fatal(err)
 		}
-		fmtc.SuccessLn("host: db")
-		fmtc.SuccessLn("name: " + projectConf["db/database"])
-		fmtc.SuccessLn("user: " + projectConf["db/user"])
-		fmtc.SuccessLn("password: " + projectConf["db/password"])
-		fmtc.SuccessLn("root password: " + projectConf["db/root_password"])
-		fmtc.SuccessLn("remote HOST:PORT: " + "localhost:" + strconv.Itoa(17000+((port-1)*20)+4))
+		fmtc.SuccessLn("First DB")
+		fmtc.SuccessLn("   host: db")
+		fmtc.SuccessLn("   name: " + projectConf["db/database"])
+		fmtc.SuccessLn("   user: " + projectConf["db/user"])
+		fmtc.SuccessLn("   password: " + projectConf["db/password"])
+		fmtc.SuccessLn("   root password: " + projectConf["db/root_password"])
+		fmtc.SuccessLn("   remote HOST:PORT: " + "localhost:" + strconv.Itoa(17000+((port-1)*20)+4))
+
+		fmt.Println("")
+		fmtc.SuccessLn("Second DB")
+		fmtc.SuccessLn("   host: db2")
+		fmtc.SuccessLn("   name: " + projectConf["db2/database"])
+		fmtc.SuccessLn("   user: " + projectConf["db2/user"])
+		fmtc.SuccessLn("   password: " + projectConf["db2/password"])
+		fmtc.SuccessLn("   root password: " + projectConf["db2/root_password"])
+		fmtc.SuccessLn("   remote HOST:PORT: " + "localhost:" + strconv.Itoa(17000+((port-1)*20)+6))
 	} else {
 		fmtc.Warning("This command is not supported for " + projectConf["platform"])
 	}
