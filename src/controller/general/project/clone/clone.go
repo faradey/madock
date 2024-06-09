815cc83a1640d64bae622a4f229a5c52a4ef3e2b
faradey <rvansp@gmail.com>
2024-06-09 23:28:41 +0300

    Clone project

--- src/controller/general/project/clone/clone.go
+++ src/controller/general/project/clone/clone.go
@@ -1,10 +1,17 @@
 package clone
 
 import (
+	"compress/gzip"
+	"github.com/faradey/madock/src/controller/general/snapshot/create"
 	"github.com/faradey/madock/src/helper/cli/arg_struct"
 	"github.com/faradey/madock/src/helper/cli/attr"
 	"github.com/faradey/madock/src/helper/cli/fmtc"
+	"github.com/faradey/madock/src/helper/configs"
+	"github.com/faradey/madock/src/helper/docker"
+	"github.com/faradey/madock/src/helper/logger"
 	"github.com/faradey/madock/src/helper/paths"
+	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -25,4 +32,99 @@ func Execute() {
 		}
 	}
 
+	projectConf := configs.GetCurrentProjectConfig()
+	exPath := paths.GetExecDirPath()
+	projectName := configs.GetProjectName()
+	currentDest := paths.MakeDirsByPath(exPath + "/projects/" + projectName + "/")
+	dest := paths.MakeDirsByPath(exPath + "/projects/" + cloneName + "/")
+	err := paths.CopyDir(currentDest, dest)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	clonePathParts := strings.Split(projectConf["path"], "/")
+	clonePath := strings.Join(clonePathParts[:len(clonePathParts)-1], "/") + "/" + cloneName + "/"
+	configs.SetParam(cloneName, "path", clonePath, projectConf["activeScope"], "")
+	cloneProjectConf := configs.GetProjectConfig(cloneName)
+	if projectConf["platform"] != "pwa" {
+		create.GetDB(projectConf, projectName, dest)
+	}
+	create.GetFiles(projectConf, projectName, dest)
+	containerName := docker.GetContainerName(cloneProjectConf, cloneName, "snapshot")
+	docker.UpSnapshot(cloneName)
+	if paths.IsFileExist(dest + "db.tar.gz") {
+		selectedFile, err := os.Open(dest + "db.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+		defer selectedFile.Close()
+		cmd := exec.Command("docker", "exec", "-i", "-u", "root", containerName, "bash", "-c", "rm -rf /var/www/mysql/* && cd /var/www/mysql && tar -zxf -")
+		out, err := gzip.NewReader(selectedFile)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		cmd.Stdin = out
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			logger.Fatal(err, containerName)
+		}
+		err = os.Remove(dest + "db.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+
+	if paths.IsFileExist(dest + "db2.tar.gz") {
+		selectedFileDb2, err := os.Open(dest + "db2.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+		defer selectedFileDb2.Close()
+		cmd := exec.Command("docker", "exec", "-i", "-u", "root", containerName, "bash", "-c", "rm -rf /var/www/mysql2/mysql/* && cd /var/www/mysql2/mysql && tar -zxf -")
+		out, err := gzip.NewReader(selectedFileDb2)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		cmd.Stdin = out
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			logger.Fatal(err, containerName)
+		}
+		err = os.Remove(dest + "db2.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+
+	if paths.IsFileExist(dest + "files.tar.gz") {
+		selectedFileFiles, err := os.Open(dest + "files.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+		defer selectedFileFiles.Close()
+		cmd := exec.Command("docker", "exec", "-i", "-u", "root", containerName, "bash", "-c", "rm -rf /var/www/html/* && cd /var/www/html && tar -zxf -")
+		out, err := gzip.NewReader(selectedFileFiles)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		cmd.Stdin = out
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			logger.Fatal(err, containerName)
+		}
+		err = os.Remove(dest + "files.tar.gz")
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}
+
+	docker.StopSnapshot(cloneName)
+
+	fmtc.SuccessLn("Project cloned successfully.\nThe new project name is " + cloneName + ".\nThe path to the project is " + clonePath)
 }
--- src/controller/general/snapshot/create/create.go
+++ src/controller/general/snapshot/create/create.go
@@ -29,41 +29,49 @@ func Execute() {
 
 	dbsPath := paths.MakeDirsByPath(dest + "/" + name + "/")
 	if projectConf["platform"] != "pwa" {
-		selectedFile, err := os.Create(dbsPath + "db.tar.gz")
+		GetDB(projectConf, projectName, dbsPath)
+	}
+	GetFiles(projectConf, projectName, dbsPath)
+	fmt.Println("Snapshot completed successfully")
+}
+
+func GetDB(projectConf map[string]string, projectName string, dbsPath string) {
+	selectedFile, err := os.Create(dbsPath + "db.tar.gz")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer selectedFile.Close()
+	writer := gzip.NewWriter(selectedFile)
+	defer writer.Close()
+	cmd := exec.Command("docker", "exec", "-i", "-u", "root", docker.GetContainerName(projectConf, projectName, "db"), "bash", "-c", "cd /var/lib/mysql && tar -czf /tmp/db.tar.gz . && cat /tmp/db.tar.gz")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = writer
+	err = cmd.Run()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	if projectConf["db2/enabled"] == "true" {
+		selectedFileDb2, err := os.Create(dbsPath + "db2.tar.gz")
 		if err != nil {
 			logger.Fatal(err)
 		}
-		defer selectedFile.Close()
-		writer := gzip.NewWriter(selectedFile)
-		defer writer.Close()
-		cmd := exec.Command("docker", "exec", "-i", "-u", "root", docker.GetContainerName(projectConf, projectName, "db"), "bash", "-c", "cd /var/lib/mysql && tar -czf /tmp/db.tar.gz . && cat /tmp/db.tar.gz")
+		defer selectedFileDb2.Close()
+		writerDb2 := gzip.NewWriter(selectedFileDb2)
+		defer writerDb2.Close()
+		cmd = exec.Command("docker", "exec", "-i", "-u", "root", docker.GetContainerName(projectConf, projectName, "db2"), "bash", "-c", "cd /var/lib/mysql && tar -czf /tmp/db2.tar.gz . && cat /tmp/db2.tar.gz")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		cmd.Stdout = writer
+		cmd.Stdout = writerDb2
 		err = cmd.Run()
 		if err != nil {
 			logger.Fatal(err)
 		}
-
-		if projectConf["db2/enabled"] == "true" {
-			selectedFileDb2, err := os.Create(dbsPath + "db2.tar.gz")
-			if err != nil {
-				logger.Fatal(err)
-			}
-			defer selectedFileDb2.Close()
-			writerDb2 := gzip.NewWriter(selectedFileDb2)
-			defer writerDb2.Close()
-			cmd = exec.Command("docker", "exec", "-i", "-u", "root", docker.GetContainerName(projectConf, projectName, "db2"), "bash", "-c", "cd /var/lib/mysql && tar -czf /tmp/db2.tar.gz . && cat /tmp/db2.tar.gz")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdout = writerDb2
-			err = cmd.Run()
-			if err != nil {
-				logger.Fatal(err)
-			}
-		}
 	}
+}
 
+func GetFiles(projectConf map[string]string, projectName string, dbsPath string) {
 	selectedFileFiles, err := os.Create(dbsPath + "files.tar.gz")
 	if err != nil {
 		logger.Fatal(err)
@@ -79,5 +87,4 @@ func Execute() {
 	if err != nil {
 		logger.Fatal(err)
 	}
-	fmt.Println("Snapshot completed successfully")
 }
