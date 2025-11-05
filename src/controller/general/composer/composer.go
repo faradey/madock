62fe0d48f728126a2f84f27d005563ac60f8ece0
faradey <rvansp@gmail.com>
2025-11-05 16:04:28 +0200

    Fixed composer install command for the shopify platform

--- src/controller/general/composer/composer.go
+++ src/controller/general/composer/composer.go
@@ -1,12 +1,13 @@
 package composer
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/faradey/madock/src/helper/cli"
 	"github.com/faradey/madock/src/helper/configs"
 	"github.com/faradey/madock/src/helper/docker"
 	"github.com/faradey/madock/src/helper/logger"
-	"os"
-	"os/exec"
 )
 
 func Execute() {
@@ -18,7 +19,9 @@ func Execute() {
 	service, user, workdir := cli.GetEnvForUserServiceWorkdir("php", "www-data", projectConf["workdir"])
 
 	if projectConf["platform"] == "shopify" {
-		workdir += "/web"
+		if _, err := os.Stat(workdir + "/composer.json"); os.IsNotExist(err) {
+			workdir += "/web"
+		}
 	}
 
 	cmd := exec.Command("docker", "exec", "-it", "-u", user, docker.GetContainerName(projectConf, projectName, service), "bash", "-c", "cd "+workdir+" && composer "+flag)
