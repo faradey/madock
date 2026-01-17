0b7eb41a7381f547b4c235f71cd58f9dd27232a7
faradey <rvansp@gmail.com>
2026-01-17 14:02:30 +0200

    Add JSON output support for CLI commands
    Add --json (-j) flag to output structured JSON for:
    - status: container states, proxy, tools status
    - config:list: project configuration
    - scope:list: available scopes with active marker
    - service:list: enabled/disabled services
    - db:info: database connection details
    
    Creates new output package for consistent JSON response format.

--- src/controller/general/scope/list/list.go
+++ src/controller/general/scope/list/list.go
@@ -2,12 +2,49 @@ package list
 
 import (
 	"fmt"
+
+	"github.com/faradey/madock/src/helper/cli/arg_struct"
+	"github.com/faradey/madock/src/helper/cli/attr"
 	"github.com/faradey/madock/src/helper/cli/fmtc"
+	"github.com/faradey/madock/src/helper/cli/output"
 	"github.com/faradey/madock/src/helper/configs"
 )
 
+type ScopeListOutput struct {
+	Scopes []ScopeInfo `json:"scopes"`
+	Active string      `json:"active"`
+}
+
+type ScopeInfo struct {
+	Name   string `json:"name"`
+	Active bool   `json:"active"`
+}
+
 func Execute() {
+	args := attr.Parse(new(arg_struct.ControllerGeneralScopeList)).(*arg_struct.ControllerGeneralScopeList)
+
 	scopes := configs.GetScopes(configs.GetProjectName())
+
+	if args.Json {
+		var scopeList []ScopeInfo
+		var activeScope string
+		for key, val := range scopes {
+			isActive := val == "1"
+			scopeList = append(scopeList, ScopeInfo{
+				Name:   key,
+				Active: isActive,
+			})
+			if isActive {
+				activeScope = key
+			}
+		}
+		output.PrintJSON(ScopeListOutput{
+			Scopes: scopeList,
+			Active: activeScope,
+		})
+		return
+	}
+
 	for key, val := range scopes {
 		fmtc.Title(key)
 		if val == "1" {
--- src/controller/general/status/status.go
+++ src/controller/general/status/status.go
@@ -3,12 +3,16 @@ package status
 import (
 	"encoding/json"
 	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/faradey/madock/src/helper/cli/arg_struct"
+	"github.com/faradey/madock/src/helper/cli/attr"
 	"github.com/faradey/madock/src/helper/cli/fmtc"
+	"github.com/faradey/madock/src/helper/cli/output"
 	"github.com/faradey/madock/src/helper/configs"
 	"github.com/faradey/madock/src/helper/logger"
 	"github.com/faradey/madock/src/helper/paths"
-	"os/exec"
-	"strings"
 )
 
 type InfoStruct struct {
@@ -18,26 +22,59 @@ type InfoStruct struct {
 	State   string `json:"State"`
 }
 
+type StatusOutput struct {
+	Services []ServiceStatus `json:"services"`
+	Proxy    []ServiceStatus `json:"proxy"`
+	Tools    ToolsStatus     `json:"tools"`
+}
+
+type ServiceStatus struct {
+	Name    string `json:"name"`
+	Service string `json:"service"`
+	State   string `json:"state"`
+	Running bool   `json:"running"`
+}
+
+type ToolsStatus struct {
+	CronEnabled     bool `json:"cron_enabled"`
+	DebuggerEnabled bool `json:"debugger_enabled"`
+}
+
 func Execute() {
+	args := attr.Parse(new(arg_struct.ControllerGeneralStatus)).(*arg_struct.ControllerGeneralStatus)
+
 	projectName := configs.GetProjectName()
 	pp := paths.NewProjectPaths(projectName)
-	cmd := exec.Command("docker", "compose", "-f", pp.DockerCompose(), "ps", "--format", "json")
-	result, err := cmd.CombinedOutput()
-	if err != nil {
-		logger.Fatal(err)
+
+	// Get services status
+	servicesData := getContainerStatus(pp.DockerCompose())
+
+	// Get proxy status
+	proxyData := getContainerStatus(paths.ProxyDockerCompose())
+
+	// Get tools status
+	projectConf := configs.GetCurrentProjectConfig()
+	toolsStatus := ToolsStatus{
+		CronEnabled:     strings.ToLower(projectConf["cron/enabled"]) == "true",
+		DebuggerEnabled: strings.ToLower(projectConf["php/xdebug/enabled"]) == "true",
 	}
 
-	if len(result) > 0 {
-		result = parseJson(result)
-		var statusData []InfoStruct
-		err = json.Unmarshal(result, &statusData)
-		if err != nil {
-			fmt.Println(err)
+	if args.Json {
+		statusOutput := StatusOutput{
+			Services: servicesData,
+			Proxy:    proxyData,
+			Tools:    toolsStatus,
 		}
-		fmtc.TitleLn("Services:")
-		for _, val := range statusData {
+		output.PrintJSON(statusOutput)
+		return
+	}
+
+	// Text output
+	fmtc.TitleLn("Services:")
+	if len(servicesData) > 0 {
+		for _, val := range servicesData {
 			row := fmt.Sprintf("%s %s", val.Service, val.State)
-			if val.State == "running" {
+			if val.Running {
 				fmtc.SuccessLn(row)
 			} else {
 				fmtc.WarningLn(row)
@@ -48,22 +85,10 @@ func Execute() {
 	}
 
 	fmtc.TitleLn("Proxy:")
-	cmd = exec.Command("docker", "compose", "-f", paths.ProxyDockerCompose(), "ps", "--format", "json")
-	result, err = cmd.CombinedOutput()
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	if len(result) > 0 {
-		result = parseJson(result)
-		var statusData []InfoStruct
-		err = json.Unmarshal(result, &statusData)
-		if err != nil {
-			fmt.Println(err)
-		}
-		for _, val := range statusData {
+	if len(proxyData) > 0 {
+		for _, val := range proxyData {
 			row := fmt.Sprintf(" %s %s", val.Service, val.State)
-			if val.State == "running" {
+			if val.Running {
 				fmtc.SuccessLn(row)
 			} else {
 				fmtc.WarningLn(row)
@@ -74,19 +99,44 @@ func Execute() {
 	}
 
 	fmtc.TitleLn("Tools:")
-	projectConf := configs.GetCurrentProjectConfig()
-
-	if strings.ToLower(projectConf["cron/enabled"]) == "true" {
+	if toolsStatus.CronEnabled {
 		fmtc.SuccessLn(" Cron is running")
 	} else {
 		fmtc.WarningLn(" Cron is not running")
 	}
 
-	if strings.ToLower(projectConf["php/xdebug/enabled"]) == "true" {
+	if toolsStatus.DebuggerEnabled {
 		fmtc.SuccessLn(" Debugger is enabled")
 	} else {
 		fmtc.WarningLn(" Debugger is disabled")
 	}
+}
+
+func getContainerStatus(composePath string) []ServiceStatus {
+	cmd := exec.Command("docker", "compose", "-f", composePath, "ps", "--format", "json")
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	var statusData []ServiceStatus
+	if len(result) > 0 {
+		result = parseJson(result)
+		var rawData []InfoStruct
+		err = json.Unmarshal(result, &rawData)
+		if err != nil {
+			return statusData
+		}
+		for _, val := range rawData {
+			statusData = append(statusData, ServiceStatus{
+				Name:    val.Name,
+				Service: val.Service,
+				State:   val.State,
+				Running: val.State == "running",
+			})
+		}
+	}
+	return statusData
 }
 
 func parseJson(data []byte) []byte {
