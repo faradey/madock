8f67d3bb9621301dc9154f83e83635db7375785d
faradey <rvansp@gmail.com>
2026-01-17 18:53:42 +0200

    Fix cron:enable/disable not saving config status
    The cron enable/disable commands now persist the cron/enabled setting
    to the project config, ensuring cron state is preserved across restarts.

--- src/controller/general/cron/cron.go
+++ src/controller/general/cron/cron.go
@@ -13,11 +13,15 @@ type ArgsStruct struct {
 func Enable() {
 	attr.Parse(new(ArgsStruct))
 	projectName := configs.GetProjectName()
+	projectConfig := configs.GetProjectConfig(projectName)
+	configs.SetParam(projectName, "cron/enabled", "true", projectConfig["activeScope"], "")
 	docker.CronExecute(projectName, true, true)
 }
 
 func Disable() {
 	attr.Parse(new(ArgsStruct))
 	projectName := configs.GetProjectName()
+	projectConfig := configs.GetProjectConfig(projectName)
+	configs.SetParam(projectName, "cron/enabled", "false", projectConfig["activeScope"], "")
 	docker.CronExecute(projectName, false, true)
 }
