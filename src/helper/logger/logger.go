23a3aacf4939ad484e7a3108b54f814c6f7bd14f
faradey <rvansp@gmail.com>
2026-02-17 19:15:27 +0200

    Make logger configurable with SetWriter and SetLogPath

--- src/helper/logger/logger.go
+++ src/helper/logger/logger.go
@@ -2,12 +2,30 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime/debug"
 	"time"
 )
+
+var (
+	customWriter io.Writer
+	customPath   string
+)
+
+// SetWriter overrides the debug log destination.
+// When set, all debug output goes to w instead of the default file.
+func SetWriter(w io.Writer) {
+	customWriter = w
+}
+
+// SetLogPath overrides the debug log file path.
+// By default, debug.log is written next to the executable.
+func SetLogPath(path string) {
+	customPath = path
+}
 
 func Fatal(v ...any) {
 	debugger(v...)
@@ -25,19 +43,20 @@ func Println(v ...any) {
 }
 
 func debugger(v ...any) {
-	var dirAbsPath string
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	msg := "\n[" + timestamp + "] " + fmt.Sprintln(v...) + string(debug.Stack())
 
-	ex, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
+	if customWriter != nil {
+		_, _ = io.WriteString(customWriter, msg)
+		return
 	}
-	exReal, err := filepath.EvalSymlinks(ex)
-	if err != nil {
-		dirAbsPath = filepath.Dir(ex)
-	} else {
-		dirAbsPath = filepath.Dir(exReal)
+
+	logPath := customPath
+	if logPath == "" {
+		logPath = defaultLogPath()
 	}
-	f, err := os.OpenFile(dirAbsPath+"/debug.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+
+	f, err := os.OpenFile(logPath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,8 +67,19 @@ func debugger(v ...any) {
 		}
 	}(f)
 
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	if _, err = f.WriteString("\n[" + timestamp + "] " + fmt.Sprintln(v...) + string(debug.Stack())); err != nil {
+	if _, err = f.WriteString(msg); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func defaultLogPath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	exReal, err := filepath.EvalSymlinks(ex)
+	if err != nil {
+		return filepath.Dir(ex) + "/debug.log"
+	}
+	return filepath.Dir(exReal) + "/debug.log"
 }
